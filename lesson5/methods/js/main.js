cb5a635835dc015e31ad9b546839a6dfa61b896c
Nikita Razlivanov <rnikitav@yandex.ru>
2020-03-27 15:55:54 +0300

    Сделал все задания

---
+++ lesson5/methods/js/main.js
@@ -0,0 +1,44 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    catalogUrl: '/catalogData.json',
+    products: [],
+    searchSubmit: false,
+    beforesearchSubmit: true,
+    isVisible: true,
+    filtredArr: [],
+    searchproducts: '',
+    imgCatalog: 'https://placehold.it/200x150'
+  },
+  methods: {
+    getJson(url){
+      return fetch(url)
+        .then(result => result.json())
+        .catch(error => {
+          console.log(error);
+        })
+    },
+    addProduct(product){
+      console.log(product.id_product);
+    },
+    searchRender() {
+        this.filtredArr
+    },
+    searchInput(event){
+      console.log(event);
+      this.filtredArr = [...this.products]
+
+      return this.filtredArr.filter(el => el.product_name.toLowerCase().indexOf(this.searchproducts.toLowerCase()) !== -1)
+    }
+  },
+  mounted(){
+    this.getJson(`${API + this.catalogUrl}`)
+      .then(data => {
+        for(let el of data){
+          this.products.push(el);
+        }
+      });
+  }
+});
